c6fe7ebc261c813f3261a2f634308b944299b840
Tashreeqlee <tashyct14@gmail.com>
2023-11-28 08:59:02 +0200

    Javascript code added

--- script/code.js
+++ script/code.js
@@ -1,1 +1,21 @@
-//
+let convertBtn = document.getElementById('convertBtn');
+let resultDiv = document.getElementById('result');
+
+convertBtn.addEventListener('click', function() {
+    let hexaInput = document.getElementById('hexaInput').value;
+    let r = parseInt(hexaInput[1] + hexaInput[2], 16);
+    let g = parseInt(hexaInput[3] + hexaInput[4], 16);
+    let b = parseInt(hexaInput[5] + hexaInput[6], 16);
+    
+    resultDiv.textContent = `RGB: ${r}, ${g}, ${b}`;
+});
+
+
+
+
+
+
+
+
+
+
